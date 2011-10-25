9aad425faa0880346874b9483aca7946efc32f8d
Mauricio Linhares <mauricio.linhares@gmail.com>
2011-10-24 21:13:00 -0300

    Mostrando os erros do formulário

--- Gemfile
+++ Gemfile
@@ -28,4 +28,3 @@ end
 
 # To use debugger
 # gem 'ruby-debug19', :require => 'ruby-debug'
-
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def error_messages_for( object, title = 'Foram encontrados erros nos seus dados' )
+    if object
+      render 'compartilhados/erros',
+             :title => title,
+             :errors => object.errors.full_messages
+    end
+  end
+
 end
